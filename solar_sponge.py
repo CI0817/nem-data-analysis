180e62363c5ae99a4374703a23fcf4ecc1129780
Chhay Irseng <chhayirseng1708@gmail.com>
2025-12-08 19:59:51 +1100

    - Convert the SETTLEMENTDATE into datetime pd object - Include the HOUR (0 to 23) in the price_data - Take the average RRP of each hour for the whole month in VIC1

--- solar_sponge.py
+++ solar_sponge.py
@@ -4,6 +4,7 @@ Focusing on data between 2025/01/01 and 2025/01/31 in Victoria.
 '''
 
 import nemosis
+import pandas as pd
 
 # Download the data
 start_time = "2025/01/01 00:00:00"
@@ -15,5 +16,17 @@ price_data = nemosis.dynamic_data_compiler(start_time, end_time, table, raw_data
                                             select_columns=['SETTLEMENTDATE', 'REGIONID','RRP'],
                                             filter_cols=['REGIONID'], filter_values=[['VIC1']])
 
+# Convert the datetime column to pd datatime object for easy extraction
+price_data['SETTLEMENTDATE'] = pd.to_datetime(price_data['SETTLEMENTDATE'])
+
+# Extract just the "Hour" (0 to 23) from the timestamp
+price_data['HOUR'] = price_data['SETTLEMENTDATE'].dt.hour
+
+# Group by hour and calculate the average price
+average_price_by_hour = price_data.groupby('HOUR')['RRP'].mean()
+
+
+
 # print(price_data)
+# print(average_price_by_hour)
 
