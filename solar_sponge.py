9574aeb66cbc72899624f742d70ddac5c1d2e6c9
Chhay Irseng <chhayirseng1708@gmail.com>
2025-12-08 20:54:42 +1100

    feat: Extend data download to October 2025 and plot average hourly spot prices for each month in subplots.

--- solar_sponge.py
+++ solar_sponge.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 # Download the data
 start_time = "2025/01/01 00:00:00"
-end_time = "2025/01/31 23:55:00"
+end_time = "2025/10/31 23:55:00"
 table = "DISPATCHPRICE"
 raw_data_cache = "./raw_data_cache"
 
@@ -20,23 +20,35 @@ price_data = nemosis.dynamic_data_compiler(start_time, end_time, table, raw_data
 # Convert the datetime column to pd datatime object for easy extraction
 price_data['SETTLEMENTDATE'] = pd.to_datetime(price_data['SETTLEMENTDATE'])
 
-# Extract just the "Hour" (0 to 23) from the timestamp
+# Extract the 'HOUR' and 'MONTH' from the 'SETTLEMENTDATE'
 price_data['HOUR'] = price_data['SETTLEMENTDATE'].dt.hour
+price_data['MONTH'] = price_data['SETTLEMENTDATE'].dt.month
 
-# Group by hour and calculate the average price
-average_price_by_hour = price_data.groupby('HOUR')['RRP'].mean()
+# Create plot with 10 subplots for the 10 months
+fig, axes = plt.subplots(5, 2, figsize=(12, 15), sharex=True, sharey=True)
+axes = axes.flatten() # flatten the axes for easier looping
 
-# Visualise the average price by hour
-plt.figure(figsize=(10,5))
-average_price_by_hour.plot(kind="line", marker="o", color="orange", linewidth=2)
+for i, month in enumerate(range(1,11)):
+    # Get the current month price data
+    monthly_data = price_data[price_data['MONTH']==month]
 
-plt.axhline(0, color="red", linestyle="--", linewidth=1, label="Zero Price Floor")
-plt.title("Average Spot Price by Hour in Victoria in January of 2025")
-plt.ylabel("Price ($/MWh)")
-plt.xlabel("Hour of Day (0-23)")
-plt.grid(True)
-plt.legend()
+    # Calculate the average price by hour for that month
+    avg_price = monthly_data.groupby('HOUR')['RRP'].mean()
 
+    # Plot the result on a specific subplot
+    ax = axes[i]
+    avg_price.plot(kind='line', marker='o', ax=ax)
+
+    # Style the plot
+    ax.set_title(f"Month {month}")
+    ax.axhline(0, color='red', linestyle='--', linewidth=1)
+    ax.grid(True)
+
+fig.supylabel("Price ($/MWh)")
+fig.supxlabel("Hour of Day (0-23)")
+fig.suptitle("Average Spot Price by Month in Victoria between Jan and Oct 2025")
+
+# plt.tight_layout()
 plt.show()
 
 # print(price_data)
